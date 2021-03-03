38522240484b2ff50eb23041aed826de3ec5f6e4
fawdlstty <f@fawdlstty.com>
2021-03-04 00:17:41 +0800

    优化引用

--- IpQuery_QQWry.hpp
+++ IpQuery_QQWry.hpp
@@ -6,19 +6,27 @@
 #include <tuple>
 #include <type_traits>
 #include <cstdint>
-#include <WinSock2.h>
+#include <cstdlib>
 
 class IpQuery_QQWry {
 public:
 	IpQuery_QQWry (std::string _file) {
 		std::ifstream ifs (_file);
-		m_bytes = std::string ((std::istreambuf_iterator<char> (ifs)), std::istreambuf_iterator<char> ());
+		//m_bytes = std::string ((std::istreambuf_iterator<char> (ifs)), std::istreambuf_iterator<char> ());
+		ifs.seekg (0, std::ios_base::end);
+		std::streampos _fsz = ifs.tellg ();
+		ifs.seekg (0, std::ios_base::beg);
+		m_bytes.resize (_fsz);
+		ifs.read (&m_bytes [0], _fsz);
 		m_index_head = read_item_uint32 (0);
 		m_index_tail = read_item_uint32 (4);
 	}
 
 	std::tuple<std::string, std::string> find_info (std::string _ip) {
-		uint32_t _nip = ::htonl (::inet_addr (_ip.c_str ()));
+		uint32_t _ip0, _ip1, _ip2, _ip3;
+		sscanf_s (_ip.data (), "%u.%u.%u.%u", &_ip0, &_ip1, &_ip2, &_ip3);
+		uint32_t _nip = (_ip0 << 24) + (_ip1 << 18) + (_ip2 << 8) + _ip3;
+		//uint32_t _nip = ::htonl (::inet_addr (_ip.data ()));
 		uint32_t _head = m_index_head;
 		uint32_t _tail = m_index_tail;
 		while (_tail > _head) {
@@ -61,10 +69,10 @@ private:
 	}
 
 	uint32_t read_item_uint32 (uint32_t _pos) {
-		uint32_t _n = (uint32_t) m_bytes [_pos];
-		_n += ((uint32_t) m_bytes [_pos + 1]) << 8;
-		_n += ((uint32_t) m_bytes [_pos + 2]) << 16;
-		_n += ((uint32_t) m_bytes [_pos + 3]) << 24;
+		uint32_t _n = 0xff & (uint32_t) m_bytes [_pos];
+		_n += (0xff & (uint32_t) m_bytes [_pos + 1]) << 8;
+		_n += (0xff & (uint32_t) m_bytes [_pos + 2]) << 16;
+		_n += (0xff & (uint32_t) m_bytes [_pos + 3]) << 24;
 		return _n;
 	}
 
